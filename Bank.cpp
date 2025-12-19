d6d22f1c17073833692910f076f22bf7ff97aaec
Akshat Patel <akshatpatel1307@gmail.com>
2025-12-19 17:05:21 +0530

    Refactor BankAccount class to bank class

--- Bank.cpp
+++ Bank.cpp
@@ -1,99 +1,73 @@
 #include <iostream>
 using namespace std;
-class BankAccount {
-private:
-    int pin;
-public:
-    int balance,accountnumber;
-    string uname,branchname ;
-    BankAccount(){
-        cout<<"a new account has been created\n";
-    }
-    BankAccount(string name, int accnum, int bal, int pincode, string branchname){
-        uname = name;
-        accountnumber = accnum;
-        balance = bal;
-        pin = pincode;
-        branchname = branchname;
-    }
-    void deposit(int amount){
-        balance += amount;
-        cout<<"Amount deposited: "<<amount<<endl;
-    }
-    void withdraw(int amount, int pincode){
-        if(pincode == pin){
-            if(amount <= balance){
-                balance -= amount;
-                cout<<"Amount withdrawn: "<<amount<<endl;
-            } else {
-                cout<<"Insufficient balance\n";
+class bank{
+    private:
+        int pin;
+    public:
+        string name,branch,ifsc;
+        int accnum,balance;
+        bank(){
+            cout<<"A new bank object is created"<<endl;
+        }
+        bank(string name, int accnum, int balance, int pin, string branch, string ifsc){
+            this->name = name;
+            this->accnum = accnum;
+            this->balance = balance;
+            this->pin = pin;
+            this->branch = branch;
+            this->ifsc = ifsc;
+        }
+        void debit(int amount, int pin){
+            if (pin==this->pin){
+                if(amount<=balance){
+                    balance=balance-amount;
+
+                    cout<<"Amount debited: "<<amount<<endl; 
+                }
             }
-        } else {
-            cout<<"Incorrect PIN\n";
-        }
-    }
-    void displayBalance(int pincode){
-        if(pincode == pin){
-            cout<<"Current balance: "<<balance<<endl;
-        } else {
-            cout<<"Incorrect PIN\n";
-        }
-    }
-    void changePin(int oldPin, int newPin){
-        if(oldPin == pin){
-            pin = newPin;
-            cout<<"PIN changed successfully\n";
-        } else {
-            cout<<"Incorrect old PIN\n";
-        }
-    }
-    void displayDetails(){
-        cout<<"Account Holder: "<<uname<<endl;
-        cout<<"Account Number: "<<accountnumber<<endl;
-        cout<<"Branch Name: "<<branchname<<endl;
-    }
-    void closeAccount(int pincode){
-        if(pincode == pin){
-            cout<<"Account closed successfully\n";
-            balance = 0;
-        } else {
-            cout<<"Incorrect PIN\n";
-        }
-    }
-    void transferFunds(BankAccount &toAccount, int amount, int pincode){
-        if(pincode == pin){
-            if(amount <= balance){
-                balance -= amount;
-                toAccount.balance += amount;
-                cout<<"Transferred "<<amount<<" to account number "<<toAccount.accountnumber<<endl;
-            } else {
-                cout<<"Insufficient balance\n";
+         else{
+                cout<<"Incorrect PIN"<<endl;
             }
-        } else {
-            cout<<"Incorrect PIN\n";
         }
-    }
-    void checkbalance(){
-        cout<<"Balance is: "<<balance<<endl;
-    }
+        void credit(int amount){
+            balance=balance+amount;
+            cout<<"Amount credited: "<<amount<<endl;
+        }
+        void checkbalance(int pin){
+            if(pin==this->pin){
+                cout<<"current balance:"<<balance<<endl;
+            }
+            else{
+                cout<<"incorrect pin please try again"<<endl;
+            }
+        }           
+        void changepin(int oldpin,int newpin){
+            if(oldpin==this->pin){
+                this->pin=newpin;
+                cout<<"PIN changed successfully"<<endl;
+            }
+            else{
+                cout<<"enter the correct pin to change the pin"<<endl;
+            }
+        }
+        void display(){
+            cout<<"Account Holder Name: "<<name<<endl;
+            cout<<"Accounr Number: "<<accnum<<endl;
+            cout<<"Branch Name: "<<branch<<endl;
+            cout<<"IFSC Code: "<<ifsc<<endl;
+        }
+    };
 
-};
+    int main() {
 
-int main() {
-    BankAccount account1("Akshat Patel", 123456, 1000, 1111, "Main Branch");
-    BankAccount account2("Akshat p", 654321, 500, 2222, "City Branch");
-
-    account1.displayDetails();
-    account1.deposit(500);
-    account1.displayBalance(1111);
-    account1.withdraw(200, 1111);
-    account1.displayBalance(1111);
-    account1.changePin(1111, 1234);
-    account1.transferFunds(account2, 300, 1234);
-    account1.displayBalance(1234);
-    account2.displayBalance(2222);
-    account1.closeAccount(1234);
-    account1.displayBalance(1234);
-
-    return 0;
-}
+        bank b1("Akshat Patel",1234,1000,1234,"khargone","KHG123");
+        b1.display();
+        b1.credit(1000);
+        b1.checkbalance(1234);
+        b1.debit(1000,1234);
+        b1.checkbalance(1234);
+        b1.changepin(1234,4321);
+        b1.checkbalance(4321);
+        
+        return 0;
+    }  
